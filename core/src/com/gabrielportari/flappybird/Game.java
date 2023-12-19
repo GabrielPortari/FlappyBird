3c2a40d3c686e7674deff129261d8c6d367d7565
Gabriel Portari <portari15@gmail.com>
2023-12-19 14:14:35 -0300

    Configuração dos canos e pontuaçao

--- core/src/com/gabrielportari/flappybird/Game.java
+++ core/src/com/gabrielportari/flappybird/Game.java
@@ -2,62 +2,126 @@ package com.gabrielportari.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class Game extends ApplicationAdapter {
 	private SpriteBatch spriteBatch;
-	private Texture[] bird;
-	private Texture background;
+	private Random random;
+	private BitmapFont textScore;
+	private int score;
+	private boolean isScore;
 
-	//Atributos de configuração
-	private int deviceHeight, deviceWidth;
+	private Texture[] bird;
 	private float birdAnimCount = 0;
 	private float gravity = 0;
 	private float birdStartHeight = 0;
+
+	private int deviceHeight, deviceWidth;
+	private Texture background;
+
+	private Texture bottomPipe, topPipe;
+	private float pipeWidth;
+	private float pipeHeight;
+	private float pipeSpacement;
+
+
 	@Override
 	public void create () {
-		spriteBatch = new SpriteBatch();
-		bird = new Texture[3];
-
-		bird[0] = new Texture("passaro1.png");
-		bird[1] = new Texture("passaro2.png");
-		bird[2] = new Texture("passaro3.png");
-		background = new Texture("fundo.png");
-
-		deviceWidth = Gdx.graphics.getWidth();
-		deviceHeight = Gdx.graphics.getHeight();
-
-		birdStartHeight = deviceHeight/2;
+		initializeTexture();
+		initializeObjects();
 	}
 
 	@Override
 	public void render () {
+		gameStateVerify();
+		scoreValidation();
+		drawTextures();
+	}
+
+	private void drawTextures(){
 		spriteBatch.begin();
 
-		// Background render
 		spriteBatch.draw(background, 0, 0, deviceWidth, deviceHeight);
 
-		// animação do passaro
-		if(birdAnimCount>3)
-			birdAnimCount = 0;
-		spriteBatch.draw(bird[(int) birdAnimCount], 100, birdStartHeight);
-		birdAnimCount += Gdx.graphics.getDeltaTime() * 8;
+		spriteBatch.draw(bird[(int) birdAnimCount], 50, birdStartHeight);
 
-		// click listener event na tela
-		if(Gdx.input.justTouched()){
-			gravity = -20;
+		spriteBatch.draw(bottomPipe, pipeWidth, deviceHeight/2 - bottomPipe.getHeight() - pipeSpacement/2 + pipeHeight);
+		spriteBatch.draw(topPipe, pipeWidth, deviceHeight/2 + pipeSpacement/2 + pipeHeight);
+
+		textScore.draw(spriteBatch, String.valueOf(score), deviceWidth/2, deviceHeight-200);
+
+		spriteBatch.end();
+	}
+
+	private void initializeTexture(){
+		bird = new Texture[3];
+		bird[0] = new Texture("passaro1.png");
+		bird[1] = new Texture("passaro2.png");
+		bird[2] = new Texture("passaro3.png");
+
+		background = new Texture("fundo.png");
+
+		bottomPipe = new Texture("cano_baixo_maior.png");
+		topPipe = new Texture("cano_topo_maior.png");
+
+	}
+
+	private void initializeObjects(){
+		spriteBatch = new SpriteBatch();
+
+		random = new Random();
+
+		deviceWidth = Gdx.graphics.getWidth();
+		deviceHeight = Gdx.graphics.getHeight();
+		birdStartHeight = deviceHeight/2;
+		pipeWidth = deviceWidth;
+		pipeSpacement = 300;
+
+		//configuração do placar
+		textScore = new BitmapFont();
+		textScore.setColor(Color.WHITE);
+		textScore.getData().setScale(10);
+	}
+
+	private void gameStateVerify(){
+		// movimentação do cano
+		pipeWidth -= Gdx.graphics.getDeltaTime() * 300; // velocidade do cano
+		if(pipeWidth < -bottomPipe.getWidth()){ // recriar o cano ao chegar no fim
+			pipeWidth = deviceWidth;
+			pipeHeight = random.nextInt(800) - 400; //variação da altura dos canos
+			isScore = false;
 		}
+		// variação bater de asas do passaro
+		birdAnimCount += Gdx.graphics.getDeltaTime() * 8;
+		if(birdAnimCount>3) {
+			birdAnimCount = 0;
+		}
+
+		// touch listener para fazer o passaro pular
+		if(Gdx.input.justTouched())
+			gravity = -20;
 
 		// aplicando gravidade no passaro
 		if(birdStartHeight > 0 || gravity < 0)
 			birdStartHeight -= gravity;
 		gravity += 1;
-
-		spriteBatch.end();
 	}
-	
+
+	private void scoreValidation(){
+		if(pipeWidth < 50-bird[0].getWidth()){
+			if(!isScore){
+				score++;
+				isScore = true;
+			}
+		}
+	}
+
 	@Override
 	public void dispose () {
 	}
