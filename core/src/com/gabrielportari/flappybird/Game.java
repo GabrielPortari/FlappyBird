5acc5f313c61e97ac43973a330e214485432c4ba
Gabriel Portari <portari15@gmail.com>
2023-12-21 14:17:54 -0300

    Configuração dos canos e pontuaçao

--- core/src/com/gabrielportari/flappybird/Game.java
+++ core/src/com/gabrielportari/flappybird/Game.java
@@ -2,10 +2,15 @@ package com.gabrielportari.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.Random;
@@ -13,23 +18,42 @@ import java.util.Random;
 public class Game extends ApplicationAdapter {
 	private SpriteBatch spriteBatch;
 	private Random random;
-	private BitmapFont textScore;
 	private int score;
 	private boolean isScore;
 
+	private int GAME_STATUS = GAME_PAUSED;
+	private static final int GAME_PAUSED = 0;
+	private static final int GAME_STARTED = 1;
+	private static final int GAME_ENDED = 2;
+
+	private BitmapFont textScore;
+	private BitmapFont textHigherScore;
+	private BitmapFont textRestart;
+
+	private ShapeRenderer shapeRenderer;
+	private Circle birdCircle;
+	private Rectangle bottomPipeRectangle;
+	private Rectangle topPipeRectangle;
+
 	private Texture[] bird;
+	private Texture background;
+	private Texture bottomPipe, topPipe;
+	private Texture gameOver;
+
 	private float birdAnimCount = 0;
 	private float gravity = 0;
 	private float birdStartHeight = 0;
+	private float birdStartWidth = 0;
 
 	private int deviceHeight, deviceWidth;
-	private Texture background;
 
-	private Texture bottomPipe, topPipe;
 	private float pipeWidth;
 	private float pipeHeight;
 	private float pipeSpacement;
 
+	private Sound flyingSound;
+	private Sound colisionSound;
+	private Sound scoreSound;
 
 	@Override
 	public void create () {
@@ -42,6 +66,7 @@ public class Game extends ApplicationAdapter {
 		gameStateVerify();
 		scoreValidation();
 		drawTextures();
+		colisionDetector();
 	}
 
 	private void drawTextures(){
@@ -49,13 +74,18 @@ public class Game extends ApplicationAdapter {
 
 		spriteBatch.draw(background, 0, 0, deviceWidth, deviceHeight);
 
-		spriteBatch.draw(bird[(int) birdAnimCount], 50, birdStartHeight);
+		spriteBatch.draw(bird[(int) birdAnimCount], 50 + birdStartWidth, birdStartHeight);
 
 		spriteBatch.draw(bottomPipe, pipeWidth, deviceHeight/2 - bottomPipe.getHeight() - pipeSpacement/2 + pipeHeight);
 		spriteBatch.draw(topPipe, pipeWidth, deviceHeight/2 + pipeSpacement/2 + pipeHeight);
 
-		textScore.draw(spriteBatch, String.valueOf(score), deviceWidth/2, deviceHeight-200);
+		textScore.draw(spriteBatch, String.valueOf(score), deviceWidth/2-40, deviceHeight-200);
 
+		if (GAME_STATUS == GAME_ENDED){
+			spriteBatch.draw(gameOver, deviceWidth/2-gameOver.getWidth()/2, deviceHeight/2-gameOver.getHeight()/2);
+			textRestart.draw(spriteBatch, "Toque para reiniciar", deviceWidth/2 - 200, deviceHeight/2 - gameOver.getHeight()/2 - 10);
+			textHigherScore.draw(spriteBatch, "Seu record é 0 pontos", deviceWidth/2 - 220, deviceHeight/2 - gameOver.getHeight() - 20);
+		}
 		spriteBatch.end();
 	}
 
@@ -70,6 +100,7 @@ public class Game extends ApplicationAdapter {
 		bottomPipe = new Texture("cano_baixo_maior.png");
 		topPipe = new Texture("cano_topo_maior.png");
 
+		gameOver = new Texture("game_over.png");
 	}
 
 	private void initializeObjects(){
@@ -87,35 +118,120 @@ public class Game extends ApplicationAdapter {
 		textScore = new BitmapFont();
 		textScore.setColor(Color.WHITE);
 		textScore.getData().setScale(10);
+
+		//texto para reiniciar
+		textRestart = new BitmapFont();
+		textRestart.setColor(Color.GREEN);
+		textRestart.getData().setScale(3);
+
+		//melhor placar
+		textHigherScore = new BitmapFont();
+		textHigherScore.setColor(Color.RED);
+		textHigherScore.getData().setScale(3);
+
+
+		//formas para colisões
+		shapeRenderer = new ShapeRenderer();
+		birdCircle = new Circle();
+		bottomPipeRectangle = new Rectangle();
+		topPipeRectangle = new Rectangle();
+
+		//inicializar sons
+		flyingSound = Gdx.audio.newSound(Gdx.files.internal("som_asa.wav"));
+		colisionSound = Gdx.audio.newSound(Gdx.files.internal("som_batida.wav"));
+		scoreSound = Gdx.audio.newSound(Gdx.files.internal("som_pontos.wav"));
 	}
 
 	private void gameStateVerify(){
-		// movimentação do cano
-		pipeWidth -= Gdx.graphics.getDeltaTime() * 300; // velocidade do cano
-		if(pipeWidth < -bottomPipe.getWidth()){ // recriar o cano ao chegar no fim
-			pipeWidth = deviceWidth;
-			pipeHeight = random.nextInt(800) - 400; //variação da altura dos canos
-			isScore = false;
+		/*
+		Status 0 - jogo pausado, esperando toque para iniciar
+		Status 1 - jogo em andamento
+		Status 2 - fim do jogo, colisão
+		 */
+		boolean screenTouch = Gdx.input.justTouched();
+		if(GAME_STATUS == GAME_PAUSED){
+			if(screenTouch){
+				gravity = -15;
+				GAME_STATUS = GAME_STARTED;
+				flyingSound.play();
+			}
+		}else if(GAME_STATUS == GAME_STARTED){
+			// movimentação do cano
+			pipeWidth -= Gdx.graphics.getDeltaTime() * 400; // velocidade do cano
+			if(pipeWidth < -bottomPipe.getWidth()){ // recriar o cano ao chegar no fim
+				pipeWidth = deviceWidth;
+				pipeHeight = random.nextInt(800) - 400; //variação da altura dos canos
+				isScore = false;
+			}
+			// aplicando gravidade no passaro
+			if(birdStartHeight > 0 || screenTouch) {
+				birdStartHeight -= gravity;
+			}
+			gravity += 1;
+
+			// touch listener para fazer o passaro pular
+			if(screenTouch) {
+				gravity = -15;
+				flyingSound.play();
+			}
+		}else if (GAME_STATUS == GAME_ENDED){
+			birdStartWidth -= Gdx.graphics.getDeltaTime()*200;
+
+			if(screenTouch) {
+				GAME_STATUS = 0;
+				score = 0;
+				gravity = 0;
+				birdStartWidth = 0;
+				birdStartHeight = deviceHeight/2;
+				pipeWidth = deviceWidth;
+			}
 		}
+
 		// variação bater de asas do passaro
 		birdAnimCount += Gdx.graphics.getDeltaTime() * 8;
 		if(birdAnimCount>3) {
 			birdAnimCount = 0;
 		}
 
-		// touch listener para fazer o passaro pular
-		if(Gdx.input.justTouched())
-			gravity = -20;
+	}
 
-		// aplicando gravidade no passaro
-		if(birdStartHeight > 0 || gravity < 0)
-			birdStartHeight -= gravity;
-		gravity += 1;
+	private void colisionDetector(){
+		/* desenho da forma do passaro */
+		int birdHeight = bird[0].getHeight()/2;
+		int birdWidth = bird[0].getWidth()/2;
+		birdCircle.set(50 + birdStartWidth + birdWidth, birdStartHeight + birdHeight, birdWidth);
+
+		/* desenho da forma dos canos */
+		int topPipeHeight = topPipe.getHeight();
+		int topPipeWidth = topPipe.getWidth();
+		topPipeRectangle.set(pipeWidth, deviceHeight/2 + pipeSpacement/2 + pipeHeight, topPipeWidth, topPipeHeight);
+
+		int bottomPipeHeight = bottomPipe.getHeight();
+		int bottomPipeWidth = bottomPipe.getWidth();
+		bottomPipeRectangle.set(pipeWidth, deviceHeight/2 - bottomPipe.getHeight() - pipeSpacement/2 + pipeHeight, bottomPipeWidth, bottomPipeHeight);
+
+		//verificação de colisão
+		boolean topColision = Intersector.overlaps(birdCircle, topPipeRectangle);
+		boolean bottomColision = Intersector.overlaps(birdCircle, bottomPipeRectangle);
+		if(topColision || bottomColision){
+			if(GAME_STATUS == GAME_STARTED){
+				GAME_STATUS = GAME_ENDED;
+				colisionSound.play();
+			}
+		}
+
+		/*shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.circle(50 + birdStartWidth + birdWidth, birdStartHeight + birdHeight, birdWidth);
+		shapeRenderer.rect(pipeWidth, deviceHeight/2 + pipeSpacement/2 + pipeHeight, topPipeWidth, topPipeHeight);
+		shapeRenderer.rect(pipeWidth, deviceHeight/2 - bottomPipe.getHeight() - pipeSpacement/2 + pipeHeight, bottomPipeWidth, bottomPipeHeight);
+		shapeRenderer.end();*/
 	}
 
 	private void scoreValidation(){
 		if(pipeWidth < 50-bird[0].getWidth()){
 			if(!isScore){
+				scoreSound.play();
 				score++;
 				isScore = true;
 			}
